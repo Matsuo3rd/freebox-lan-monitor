f5c8aca41d2a417cf86ee1a27014dd660f5233fc
Bertrand Martel <matsuo@free.fr>
2022-07-02 00:10:15 +0200

    Update freebox-lan-monitor.sh
    . Linux Service notify support
    . Resilient to Freebox Server reboot
    . Log archiver
    . Typos / cleanup

--- freebox-lan-monitor.sh
+++ freebox-lan-monitor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOG_FILES_KEPT=5
+
 FREEBOX_API_VERSION=v8
 FREEBOX_APP_NAME="Freebox Server LAN Monitor"
 FREEBOX_APP_ID="freebox_server_lan_monitor"
@@ -10,6 +12,7 @@ FREEBOX_API_WS_BASE_URL="ws://${FREEBOX_APP_HOSTNAME}/api/${FREEBOX_API_VERSION}
 CURRENT_DIR=$(cd "$(dirname "$0")"; pwd)
 PATH=$PATH:"${CURRENT_DIR}"
 LOG_DIR="${CURRENT_DIR}/log"
+LOG_FILE="${LOG_DIR}/freebox-lan-monitor.log"
 FREEBOX_API_CONFIG_DIR="${CURRENT_DIR}/config"
 FREEBOX_API_TMP_DIR="${CURRENT_DIR}/tmp"
 
@@ -35,15 +38,16 @@ main() {
 	mkdir -p "$FREEBOX_API_TMP_DIR"
 	mkdir -p "$LOG_DIR"
 
-	trap "quit" INT TERM EXIT
+	#TODO: systemctl stop is not trapped
+	trap "quit" SIGINT SIGTERM SIGEXIT
 
-	exec &> >(tee -a "${LOG_DIR}/freebox-lan-monitor.log")
-	
+	exec &> >(tee -a -i "${LOG_FILE}")
+
 	log "Starting freebox-lan-monitor"
 
 	if [[ ! -z "$MOSQUITTO_PUB" ]]; then
 		if [ -f "${FREEBOX_API_CONFIG_DIR}/mqtt_config.json" ]; then
-			log "Loading MQTT config in ${FREEBOX_API_CONFIG_DIR}/mqtt_config.json"
+			log "Loading MQTT config from file ${FREEBOX_API_CONFIG_DIR}/mqtt_config.json"
 			MOSQUITTO_PUB_PARAMS=$(cat "${FREEBOX_API_CONFIG_DIR}/mqtt_config.json" | parseJSON ".mosquitto_pub_params")
 			MQTT_TOPIC=$(cat "${FREEBOX_API_CONFIG_DIR}/mqtt_config.json" | parseJSON ".mqtt_topic")
 
@@ -58,11 +62,17 @@ main() {
 	login
 
 	browseLAN
+
+	if [[ ! -z $NOTIFY_SOCKET ]]; then
+		systemd-notify --ready
+	fi
+
+	logArchiver &
+
 	monitorLAN
 }
 
 requestAuthorization () {
-	# 1ere etape: on recupere le token de l'appli. Uniquement au 1er passage.
 	if [ ! -f ${FREEBOX_API_CONFIG_DIR}/app_params.json ] ; then
 		echo { > ${FREEBOX_API_CONFIG_DIR}/app_params.json
 		echo "\"app_id\": \"${FREEBOX_APP_ID}\"," >> ${FREEBOX_API_CONFIG_DIR}/app_params.json
@@ -72,7 +82,6 @@ requestAuthorization () {
 		echo } >> ${FREEBOX_API_CONFIG_DIR}/app_params.json
 	fi
 
-	# On recupere l'app_token au premier passage - nécessite action sur la freebox
 	if [ ! -f ${FREEBOX_API_CONFIG_DIR}/app_token.json ] ; then
 		${CURL} -s -H "Content-Type: application/json" --data @${FREEBOX_API_CONFIG_DIR}/app_params.json ${FREEBOX_API_BASE_URL}/login/authorize/ > ${FREEBOX_API_CONFIG_DIR}/app_token.json
 	fi
@@ -89,7 +98,6 @@ requestAuthorization () {
 	app_token=${appToken[0]}
 	track_id=${appToken[1]}
 
-	# (2ème étape: Monitorer le track_id)
 	status=""
 	if [ ! -f ${FREEBOX_API_TMP_DIR}/status.json ] ; then
 		while [ "$status" != "granted" ]
@@ -98,54 +106,43 @@ requestAuthorization () {
 			status=$(cat ${FREEBOX_API_TMP_DIR}/status.json | parseJSON ".result.status")			
 
 			case $status in
-		    granted)
-		      log "The App Token is valid and can be used to open a session"
-		      ;;
-		    pending)
-		      warn "Pending App Token access grant. Please grant access from the Freebox front panel."
+				granted)
+					log "App Token is valid and can be used to open a session"
+				;;
+				pending)
+					warn "Pending App Token access grant. Please grant access from the Freebox front panel."
 					sleep 5
-		      ;;
-		    timeout)
-		      error "App access grant timeout. Aborting."
+				;;
+				timeout)
+					error "App access grant timeout. Aborting."
 					exit 1
-		      ;;
-		    unknown)
-		      error "The App Token is invalid or has been revoked. Aborting."
+				;;
+				unknown)
+					error "The App Token is invalid or has been revoked. Aborting."
 					exit 1
-		      ;;
-		    denied)
-		      error "The user denied the authorization request. Aborting."
+				;;
+				denied)
+					error "The user denied the authorization request. Aborting."
 					exit 1
-		      ;;
-		    *)
-		      error "Unexpected app authorization status error: $status. Aborting."
-		      cat ${FREEBOX_API_TMP_DIR}/status.json
-		      exit 1
-		      ;;
-		  esac
+				;;
+				*)
+					error "Unexpected app authorization status error: $status. Aborting. Message: $(cat "${FREEBOX_API_TMP_DIR}/status.json")"
+					exit 1
+				;;
+			esac
 		done
 	fi
 }
 
 login() {
-	# 3ème étape: Obtenir un session_token, (challenge + app_token = password)
-	#on recupere le challenge
 	${CURL} -s -H "Content-Type: application/json" ${FREEBOX_API_BASE_URL}/login/ > ${FREEBOX_API_TMP_DIR}/login.json
 	challenge=$(cat ${FREEBOX_API_TMP_DIR}/login.json | parseJSON ".result.challenge")
-
-	# Construction du password à partir du app_token + challenge
 	password=$(echo -n $challenge | openssl sha1 -hmac $app_token | cut -d '=' -f2 | sed 's/ //g')
 
-	# 4ème étape: On ouvre la session
 	echo { > ${FREEBOX_API_TMP_DIR}/credentials.json
 	echo '"app_id": "'$FREEBOX_APP_ID'",' >> ${FREEBOX_API_TMP_DIR}/credentials.json
 	echo '"password": "'$password'"' >> ${FREEBOX_API_TMP_DIR}/credentials.json
 	echo } >> ${FREEBOX_API_TMP_DIR}/credentials.json
-	 
-	# Pour voir le contenu de l'échange
-	#${CURL} --trace-ascii -d var=val ${FREEBOX_API_BASE_URL}/login/session/
-
-	#cat ${FREEBOX_API_TMP_DIR}/credentials.json
 
 	${CURL} -s -H "Content-Type: application/json" --data @${FREEBOX_API_TMP_DIR}/credentials.json ${FREEBOX_API_BASE_URL}/login/session/ > ${FREEBOX_API_TMP_DIR}/session.json
 	session=($(cat ${FREEBOX_API_TMP_DIR}/session.json | parseJSON ".success, .result.session_token"))
@@ -167,34 +164,53 @@ browseLAN() {
 
 	echo ${response} | parseJSON ".result[]" | while read result; do
 
-    primary_name=$(normalizePrimaryName "$(echo ${result} | parseJSON ".primary_name")")
-    if [[ ! -z "$primary_name" ]]; then
-    	mqtt_msg="{\"success\": true, \"result\": ${result}, \"source\": \"lan\", \"action\": \"browse\"}"
-    	if [[ -f "${FREEBOX_API_CONFIG_DIR}/mqtt_config.json" ]]; then
+		primary_name=$(normalizePrimaryName "$(echo ${result} | parseJSON ".primary_name")")
+		if [[ ! -z "$primary_name" ]]; then
+			mqtt_msg="{\"success\": true, \"result\": ${result}, \"source\": \"lan\", \"action\": \"browse\"}"
+			if [[ -f "${FREEBOX_API_CONFIG_DIR}/mqtt_config.json" ]]; then
 				${MOSQUITTO_PUB} ${MOSQUITTO_PUB_PARAMS} -t "${MQTT_TOPIC}/${primary_name}" -m "${mqtt_msg}" &
 			fi
-    else
-    	warn "Ignoring empty primary_name. ${result}"
-    fi
+		else
+			warn "Ignoring empty primary_name. ${result}"
+		fi
 		
 	done
 
 	log "LAN devices browsing completed"
 }
 
+#| ${WEBSOCAT} -t --no-close -H="X-Fbx-App-Auth: ${session_token}" "${FREEBOX_API_WS_BASE_URL}/ws/event" \
+
+#| ${WEBSOCAT} -t --no-close -H="X-Fbx-App-Auth: ${session_token}" - "autoreconnect:${FREEBOX_API_WS_BASE_URL}/ws/event" \
+
+#| ${WEBSOCAT} --text --no-close -H=X-Fbx-App-Auth:${session_token} ${FREEBOX_API_WS_BASE_URL}/ws/event \
+
 monitorLAN() {
+	log "LAN devices monitoring registration in progress"
+	#TODO: try reconnect:ws://....
 	echo '{"action": "register", "events": ["lan_host_l3addr_reachable", "lan_host_l3addr_unreachable"]}' \
-		| ${WEBSOCAT} -t --no-close -H="X-Fbx-App-Auth: ${session_token}" "${FREEBOX_API_WS_BASE_URL}/ws/event" \
+		| ${WEBSOCAT} --text --no-close -H="X-Fbx-App-Auth: ${session_token}" ${FREEBOX_API_WS_BASE_URL}/ws/event\
 		| while read notification; do
+			#log "${notification}"
 			processLANNotification "${notification}"
 	done
+
+	log "LAN devices monitoring stopped. Relaunching."
+	while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' --connect-timeout 5 ${FREEBOX_APP_HOSTNAME})" != "200" ]];
+	do
+			warn "Freebox endpoint could not be reached. Retrying ..."
+			sleep 5
+	done
+
+	login
+	monitorLAN
 }
 
 processLANNotification() {
-	#https://stackoverflow.com/questions/43291389/using-jq-to-assign-multiple-output-variables
+	# https://stackoverflow.com/questions/43291389/using-jq-to-assign-multiple-output-variables
 	read action success < <(echo $(echo $1 | parseJSON ".action, .success"))
 	case $action in
-	  register)
+		register)
 			if [[ "$success" == "true" ]]; then
 				log "LAN devices monitoring registration successful"
 			else
@@ -202,8 +218,8 @@ processLANNotification() {
 				error "LAN devices monitoring registration failed: ${msg}"
 				exit 1
 			fi
-	    ;;
-	  notification)
+		;;
+		notification)
 			# primary_name set last on purpose as it may contain white space and break multiple variable parsing
 			read event reachable primary_name < <(echo $(echo $1 | parseJSON ".event, .result.reachable, .result.primary_name"))
 			access_point=$(echo $1 | parseJSON ".result.access_point")
@@ -218,22 +234,20 @@ processLANNotification() {
 			log "$primary_name:$event"
 
 			if [[ ! -z "$primary_name" ]]; then
-	    	if [[ -f "${FREEBOX_API_CONFIG_DIR}/mqtt_config.json" ]]; then
+				if [[ -f "${FREEBOX_API_CONFIG_DIR}/mqtt_config.json" ]]; then
 					${MOSQUITTO_PUB} ${MOSQUITTO_PUB_PARAMS} -t "${MQTT_TOPIC}/${primary_name}" -m "${1}" &
 					${MOSQUITTO_PUB} ${MOSQUITTO_PUB_PARAMS} -t "${MQTT_TOPIC}/${primary_name}/last_event" -m "${event}" &
 					${MOSQUITTO_PUB} ${MOSQUITTO_PUB_PARAMS} -t "${MQTT_TOPIC}/${primary_name}/last_event_timestamp" -m "$(date +"%Y-%m-%dT%H:%M:%S")" &
 					${MOSQUITTO_PUB} ${MOSQUITTO_PUB_PARAMS} -t "${MQTT_TOPIC}/${primary_name}/reachable" -m "${reachable}" &
 					${MOSQUITTO_PUB} ${MOSQUITTO_PUB_PARAMS} -t "${MQTT_TOPIC}/${primary_name}/access_point" -m "${access_point}" &
-
 				fi
-	    else
-	    	warn "Ignoring empty primary_name. ${1}"
-	    fi
-
-	    ;;
-	  *)
-	    error "Unexpected action: '$action'"
-	    ;;
+			else
+				warn "Ignoring empty primary_name. ${1}"
+			fi
+		;;
+		*)
+			error "Unexpected action: '$action'. Message: $1"
+		;;
 	esac
 }
 
@@ -241,15 +255,26 @@ normalizePrimaryName() {
 	echo "${1}" | iconv -f utf8 -t ascii//TRANSLIT | tr A-Z a-z | tr [:blank:] _ | tr -cd [:alnum:]_ | sed 's/[^a-zA-Z 0-9 _-]//g'
 }
 
+logArchiver() {
+	while true
+	do
+		sleep 1d
+		log "Archiving logs"
+		${ZIP} "${LOG_FILE}_$(date +"%Y-%m-%d_%H%M%S").zip" "${LOG_FILE}" &> /dev/null
+		# Clear log file content instead of deleting it - which messes with log process
+		> "${LOG_FILE}"
+		# Delete old archives
+		ls -t "${LOG_DIR}/"*.zip | tail -n +$(($LOG_FILES_KEPT + 1)) | while read logArchive; do rm "${logArchive}"; done
+	done
+}
+
 quit() {
-	log "Stoping freebox-lan-monitor"
-	#exec 2>&3 # restore backup of original FD 3, so tee and grep can exit
+	log "Stopping freebox-lan-monitor"
+	exit
 }
 
 logout() {
 	error "not implemented"
-
-	# 6ème étape: Fermer la session (marche pas; normalement, en post, sans datas ? )
 	#wget --header='X-Fbx-App-Auth: '$session_token ${FREEBOX_API_BASE_URL}/login/logout/ -O ${FREEBOX_API_TMP_DIR}/logout.json
 }
 
